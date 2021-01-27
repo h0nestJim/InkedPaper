25be678787811df9ac016781190324ecd4aef512
Jim Shaun <jim@jimsmacook15pro.home>
2021-01-27 15:22:26 +0000

    Began to implement Service Worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,10 @@ import store from './store'
 import './bootstrap.min.css'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import reportWebVitals from './reportWebVitals';
+
+
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,7 +17,10 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorkerRegistration.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
